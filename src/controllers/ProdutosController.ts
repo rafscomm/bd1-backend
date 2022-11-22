6ae9f6ced52b4058f990a9708bcdc550b0d17ed5
Emmanoel <emmanoelsveiga@gmail.com>
2022-11-22 19:10:05 -0300

    :sparkles: Criando criação de produto

--- src/controllers/ProdutosController.ts
+++ src/controllers/ProdutosController.ts
@@ -5,3 +5,18 @@ export const produtosController = async (request: Request, response: Response) =
   const produtos = await prisma.produto.findMany();
   return response.status(201).json(produtos);
 }
+
+export const criarProdutos = async (request: Request, response: Response) => {
+  const {nome, marca, modelo, price, imageURL} = request.body;
+
+  const produto = await prisma.produto.create({
+    data: {
+      nome:nome,
+      marca: marca,
+      modelo: modelo,
+      price: price,
+      imageURL: imageURL,
+    }
+  });
+  return response.status(201).json(produto);
+}
