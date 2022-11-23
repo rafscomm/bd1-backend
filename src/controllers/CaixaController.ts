8146b512a7e23fd8b57d21f09d643d59db1be4f1
Emmanoel <emmanoelsveiga@gmail.com>
2022-11-23 01:26:22 -0300

    :sparkles: adioonando include de clientes

--- src/controllers/CaixaController.ts
+++ src/controllers/CaixaController.ts
@@ -5,3 +5,11 @@ export const caixaController = async (request: Request, response: Response) => {
   const caixas = await prisma.caixa.findMany();
   return response.status(201).json(caixas);
 }
+
+export const buscaVendas = async (request: Request, response: Response) => {
+  const {id} = request.params;
+  const vendas = await prisma.pagamento.findMany({
+
+  });
+  return response.status(201).json(vendas);
+}
